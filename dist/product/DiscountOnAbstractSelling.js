576f8d891be172098a3e3449af4c8448e4f2f62c
Dima163163 <khlopovdima@gmail.com>
2024-02-26 19:33:22 +0400

    TS-lesson4

--- dist/product/DiscountOnAbstractSelling.js
+++ dist/product/DiscountOnAbstractSelling.js
@@ -12,7 +12,7 @@ class DiscountOnAbstractSelling extends AbstractSelling_1.AbstractSelling {
     }
     getPrice() {
         if (this.isDiscount) {
-            return Number((this.product.priceProduct * this.count * 0.9).toFixed(2));
+            return Number((this.product.priceProduct * this.count * (1 - 10 / 100)).toFixed(2));
         }
         else {
             return Number((this.product.priceProduct * this.count).toFixed(2));
